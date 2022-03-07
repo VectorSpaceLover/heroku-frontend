35e167f14ffd695e218bd494e31375cd4d62ea9b
FullStack developer <90791245+Venus9023@users.noreply.github.com>
2022-03-07 09:02:29 +0900

    Update index.js

--- src/pages/collection/index.js
+++ src/pages/collection/index.js
@@ -25,66 +25,7 @@ export default function Collection(){
     return(
         <Styles>
             <div className="collection-container">
-                <div className="search-bar">
-                    <div className="topic-txt">
-                        {`Collctins (0)`}
-                    </div>
-                    <div className="search-action">
-                        <SearchBox />
-                        <CustomedButton text={"Add Collection"} onClick={handleClickOpen}/>
-                    </div>
-                </div>
-                <div className="collection-list">
-                    <Grid container spacing={3}>
-                        {collections && collections.map((info, idx) => {
-                            return (
-                                <Grid item sm={4} xs={6} md={3} key={idx}>
-                                    <CollectionCard info={info} key={idx}/>
-                                </Grid>
-                            )
-                        })}
-                    </Grid>
-                </div>
-                <Dialog
-                    open={open} 
-                    onClose={handleClose}
-                    maxWidth='md'
-                    fullWidth={true}
-                    PaperProps={{
-                        style: {
-                          borderRadius: 24,
-                          overflow: 'hidden',
-                          backgroundColor: 'transparent',
-                          boxShadow: 'none',
-                          padding: 30,
-                          '@media(minWidth: 780px)' : {
-                            height: 486,
-                          }
-                        },
-                    }}
-                >
-                    <DialogStyles>
-                        <div className="dialog-container">
-                            <div className="header">
-                                Create a new collection
-                            </div>
-                            <div className="body">
-                                <div className="label">
-                                    Name
-                                </div>
-                                <Input />
-                                <div className="label">
-                                    Description (optional)
-                                </div>
-                                <EmailInput />
-                            </div>
-                            <div className="footer">
-                                <TextButton text={"Create Colleciton"}/>
-                            </div>
-                            <CloseButton handleClose={handleClose}/>
-                        </div>
-                    </DialogStyles>
-                </Dialog>
+                
             </div>
         </Styles>
     )
